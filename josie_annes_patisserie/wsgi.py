d8231d21baa45041f5ed5b51a86b045fd70ea019
Sean <seanpatmeade@gmail.com>
2022-08-25 17:44:52 +0100

    Set up local venv and have django app set and running locally in browser when runserver is ran

---
+++ josie_annes_patisserie/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for josie_annes_patisserie project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'josie_annes_patisserie.settings')
+
+application = get_wsgi_application()
