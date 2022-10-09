ed0441d478245b17baf560efda7cbbaa9e76e968
Sean <seanpatmeade@gmail.com>
2022-10-09 15:14:25 +0100

    Add first 3 products to home page

--- home/views.py
+++ home/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from products.models import Product
+
 
 # Create your views here.
 def index(request):
     """ A view to return index page """
-    return render(request, 'home/index.html')
+    products = Product.objects.all()[:3]
+
+    context = {
+        'products': products,
+    }
+    return render(request, 'home/index.html', context)
