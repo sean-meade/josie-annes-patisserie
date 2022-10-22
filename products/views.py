345686436078659b1087f4072319469b0f9ecd3f
Sean <seanpatmeade@gmail.com>
2022-10-22 15:43:20 +0100

    create search and filter products and seperate out into collection or delivery

--- products/views.py
+++ products/views.py
@@ -1,16 +1,35 @@
-from django.shortcuts import render, get_object_or_404
-from products.models import Product
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from products.models import Product, Category
 
 
 def all_products(request):
     """ A view to show all products, including sorting search filtering """
 
     products = Product.objects.all()
+    query = None
+    categories = None
 
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__friendly_name__in= categories)
+            categories = Category.objects.filter(friendly_name__in=categories)
 
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter anything to search")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query) | Q(ingredients__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
