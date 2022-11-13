9872aace6a428bccc877232ef8c76f1576a57778
Sean <seanpatmeade@gmail.com>
2022-11-13 11:40:31 +0000

    Add functionality for deleting products

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('<str:sku>/', views.find_product, name='product_detail'),
     path('add/', views.add_product, name='add_product'),
     path('edit/<str:sku>/', views.edit_product, name='edit_product'),
+    path('delete/<str:sku>/', views.delete_product, name='delete_product'),
 ]
--- products/views.py
+++ products/views.py
@@ -71,9 +71,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.sku]))
         else:
             messages.error(request, 'Failed to add product. Please ensure the form is valid.')
     else:
@@ -108,3 +108,11 @@ def edit_product(request, sku):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, sku):
+    """ Delete a product from the store """
+    product = get_object_or_404(Product, sku=sku)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
