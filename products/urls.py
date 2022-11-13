0c465bf129966a09f1b311572866056562cb0ae1
Sean <seanpatmeade@gmail.com>
2022-11-13 10:33:51 +0000

    Editing Products

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('<int:sku>/', views.find_product, name='product_detail'),
+    path('<str:sku>/', views.find_product, name='product_detail'),
     path('add/', views.add_product, name='add_product'),
+    path('edit/<str:sku>/', views.edit_product, name='edit_product'),
 ]
