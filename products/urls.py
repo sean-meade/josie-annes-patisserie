48c2d35d4f575cd74fabb3c485f5dc0841521f5b
Sean <seanpatmeade@gmail.com>
2022-11-15 10:41:29 +0000

    Make shopping bag responsive on mobile

--- products/urls.py
+++ products/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('<str:sku>/', views.find_product, name='product_detail'),
     path('add/', views.add_product, name='add_product'),
+    path('<str:sku>/', views.find_product, name='product_detail'),
     path('edit/<str:sku>/', views.edit_product, name='edit_product'),
     path('delete/<str:sku>/', views.delete_product, name='delete_product'),
 ]
