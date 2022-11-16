83813dcc4c2e96abe1770ebd78e2cc1e9816bd3f
Sean <seanpatmeade@gmail.com>
2022-11-16 20:16:56 +0000

    create cake_order app

---
+++ cake_order/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CakeOrderConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'cake_order'
---
+++ cake_order/models.py
@@ -0,0 +1,46 @@
+import uuid
+
+from django.db import models
+from profiles.models import UserProfile
+import datetime
+
+
+three_weeks_from_now = datetime.date.today() + datetime.timedelta(days=21)
+
+DATE_CHOICES = []
+
+TIME_CHOICES = (
+        ('12:00', '12:00'),
+        ('13:00', '13:00'),
+        ('14:00', '14:00'),
+        ('15:00', '15:00'),
+    )
+
+
+class AfternoonTea(models.Model):
+    cake_order_number = models.CharField(max_length=32, null=False, editable=False)
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    date = models.DateField(auto_now_add=False)
+    notes = models.TextField()
+    under_review = models.BooleanField(default=True)
+
+
+    def _generate_cake_order_number(self):
+        """
+        Generate a random, unique order number using UUID
+        """
+        return uuid.uuid4().hex.upper()
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the order number
+        if it hasn't been set already.
+        """
+        if not self.booking_number:
+            self.booking_number = self._generate_cake_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.booking_number
