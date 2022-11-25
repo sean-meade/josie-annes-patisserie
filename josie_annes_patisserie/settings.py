42c21b89a7b07104c014b778c92224d0681e1e58
Sean <seanpatmeade@gmail.com>
2022-11-25 18:19:55 +0000

    add allowed host from render

--- josie_annes_patisserie/settings.py
+++ josie_annes_patisserie/settings.py
@@ -13,6 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = ["josie-annes-patisserie.herokuapp.com", "127.0.0.1"]
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
