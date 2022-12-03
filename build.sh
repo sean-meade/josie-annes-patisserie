79dc5429b96826b34b9e5b950671ffaf4ffcdf61
Sean <seanpatmeade@gmail.com>
2022-12-03 15:29:26 +0000

    Add createsuperuser to build script

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
  pip install -r requirements.txt
  python manage.py collectstatic --noinput
  python manage.py makemigrations && python manage.py migrate
+ python manage.py createsuperuser --no-input
