c5f7e753ea419e85901d67135e4638fd928b738a
Sean <seanpatmeade@gmail.com>
2022-12-08 10:56:10 +0000

    make sku mandatory

--- products/models.py
+++ products/models.py
@@ -34,7 +34,7 @@ class Allergens(models.Model):
 
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=254, null=True, blank=True)
+    sku = models.CharField(max_length=254, null=False, blank=False)
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
