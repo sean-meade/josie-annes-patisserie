f3544c2249944d28413d1d2d779fd2c6676f9684
Sean <seanpatmeade@gmail.com>
2022-12-12 19:05:12 +0000

    remove creation of superuser from build file

--- build.sh
+++ build.sh
@@ -2,4 +2,3 @@
  pip install -r requirements.txt
  python manage.py collectstatic --noinput
  python manage.py makemigrations && python manage.py migrate
- python manage.py createsuperuser --no-input
