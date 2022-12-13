e8df7b78294a0d76830f0c7afcec54a673bf7921
Sean <seanpatmeade@gmail.com>
2022-12-13 12:40:18 +0000

    handle_payment_intent_succeeded update for payload

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -48,20 +48,15 @@ class StripeWH_Handler:
         """
         Handle the payment_intent.succeeded webhook from Stripe
         """
-        intent = event.data.object
+        intent = event.charges.data
         print(f"This is the intent {intent}")
-        pid = intent.id
-        bag = intent.metadata.bag
-        save_info = intent.metadata.save_info
+        pid = intent[0].id
+        bag = intent[0].metadata.bag
+        save_info = intent[0].metadata.save_info
 
-        # Get the Charge object
-        stripe_charge = stripe.Charge.retrieve(
-            intent.latest_charge
-        )
-
-        billing_details = stripe_charge.billing_details
-        shipping_details = intent.shipping
-        grand_total = round(stripe_charge.amount / 100, 2)
+        billing_details = intent[0].billing_details
+        shipping_details = intent[0].shipping
+        grand_total = round(intent[0].amount / 100, 2)
 
         # Clean data in the shipping details
         for field, value in shipping_details.address.items():
