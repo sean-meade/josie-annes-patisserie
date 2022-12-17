d0ebda4781d5f2595fcf3bc9822456a9be1838d6
Sean <seanpatmeade@gmail.com>
2022-12-17 22:43:29 +0000

    fix webhook_handler.py

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -70,7 +70,7 @@ class StripeWH_Handler:
             if save_info:
                 profile.default_phone_number = shipping_details.phone
                 profile.default_country = shipping_details.address.country
-                profile.default_eircode = shipping_details.address.eircode_code
+                profile.default_eircode = shipping_details.address.postal_code
                 profile.default_town_or_city = shipping_details.address.city
                 profile.default_street_address1 = shipping_details.address.line1
                 profile.default_street_address2 = shipping_details.address.line2
