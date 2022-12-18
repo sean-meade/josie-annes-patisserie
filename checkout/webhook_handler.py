84c34ad97dc40920b591ae17c3a84be29443b15b
Sean <seanpatmeade@gmail.com>
2022-12-18 17:20:54 +0000

    Adding items to email

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -26,7 +26,7 @@ class StripeWH_Handler:
             {'order': order})
         body = render_to_string(
             'checkout/confirmation_email_body.txt',
-            {'order': order, 'items': str(items), 'contact_email': settings.DEFAULT_FROM_EMAIL})
+            {'order': order, 'items': items, 'contact_email': settings.DEFAULT_FROM_EMAIL})
 
         send_mail(
             subject,
