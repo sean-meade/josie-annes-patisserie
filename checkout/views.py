d033250bd6157192f655836b911408ee20a2575b
Sean <seanpatmeade@gmail.com>
2023-02-04 10:17:15 +0000

    fix for internal server error

--- checkout/views.py
+++ checkout/views.py
@@ -106,7 +106,7 @@ def checkout(request):
                     'email': profile.user.email,
                     'phone_number': profile.default_phone_number,
                     'country': profile.default_country,
-                    'eircode': profile.default_eircode,
+                    'postal_code': profile.default_eircode,
                     'town_or_city': profile.default_town_or_city,
                     'street_address1': profile.default_street_address1,
                     'street_address2': profile.default_street_address2,
