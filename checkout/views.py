4f226871a6cc6f590eb483a601c3ede08db8b59a
Sean <seanpatmeade@gmail.com>
2023-02-04 21:38:32 +0000

    update requirements.txt and put print in to check if order is requested

--- checkout/views.py
+++ checkout/views.py
@@ -136,7 +136,8 @@ def checkout_success(request, order_number):
     Handle successful checkouts
     """
     save_info = request.session.get('save_info')
-    order = get_object_or_404(Order, order_number=order_number)
+    order = Order.objects.get(order_number=order_number)
+    print(order.name)
 
     if request.user.is_authenticated:
         profile = UserProfile.objects.get(user=request.user)
