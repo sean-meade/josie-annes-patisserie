cccc463c0f640688e41688b71c81c2b004c3d4b6
Sean <seanpatmeade@gmail.com>
2023-02-04 22:26:29 +0000

    user already created

--- build.sh
+++ build.sh
@@ -2,4 +2,3 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py makemigrations && python manage.py migrate
-python manage.py createsuperuser --no-input
