afe91875bb289cadb9a349fa0566b82d1dccca52
Sean <seanpatmeade@gmail.com>
2023-02-04 15:12:19 +0000

    404 is hardly this easy?

--- josie_annes_patisserie/urls.py
+++ josie_annes_patisserie/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import handler404
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
@@ -16,5 +15,3 @@ urlpatterns = [
                   path('afternoon_tea/', include('afternoon_tea.urls')),
                   path('cake_order/', include('cake_order.urls')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-handler404 = 'josie_annes_patisserie.views.error_404'
--- josie_annes_patisserie/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-
-def error_404(request, exception):
-    """ Error Handler 404 - Page Not Found """
-    return render(request, "errors/404.html", status=404)
